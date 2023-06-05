1a7127947540786ff10d7ccf1d5b4fbaf8b77e61
Aditya kumar <Adityakumar14022002@gmail.com>
2023-06-05 14:55:34 +0530

    add loding window and some change is files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// var loder = document.querySelector("#loder");
+// loder.addEventListener("load", function(){
+//    setTimeout(() => {
+//      loder.display= "none"
+//    }, 2222);
+// })
+
+// $(window).on("load",function(){
+//     $("#loder").fadeOut("slow");
+// });
+
+var loding = document.getElementById("loding");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loding.style.display = "none";
+  },4355);
+});
